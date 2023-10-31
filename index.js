61cdc8e75e87a518130c9e193221d1af344707cc
hmdmasudsarder <hmmasudsarder@gmail.com>
2023-10-31 23:55:32 +0600

    change patch

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     await client.connect();
 
     const servicesCollection = client.db('carDoctor').collection('services')
+    const bookingCollection = client.db('carDoctor').collection('bookings');
 
 
     app.get('/services', async(req, res)=>{
@@ -40,9 +41,47 @@ async function run() {
     app.get('/services/:id', async(req, res) =>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
-        const result = await servicesCollection.findOne(query);
+        const options = {
+          projection: {title:1, price:1, service_id:1, img: 1}
+        };
+        const result = await servicesCollection.findOne(query, options);
         res.send(result)
+    });
+
+    app.get('/bookings', async(req, res) => {
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
     })
+    app.post('/bookings', async(req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.patch('/bookings/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const query = req.body;
+      const updateDoc = {
+        $set: {
+          status: query.status
+        }
+      }
+      const result = await bookingCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
